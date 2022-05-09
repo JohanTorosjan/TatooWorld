a0cc182fb8a6fb228f568296d545da4c6ffd3192
JohanTorosjan <johan.torosjan@etu.umontpellier.fr>
2022-05-10 00:34:55 +0200

    fc

---
+++ db.js
@@ -0,0 +1,26 @@
+const mysql = require("mysql");
+
+const dbConfig = require("./config/db.config.js");
+
+
+
+// Create a connection to the database
+const sql =mysql.createPool({
+  host: dbConfig.HOST,
+  user: dbConfig.USER,
+  password: dbConfig.PASSWORD,
+  database: dbConfig.DB
+});
+
+sql.query(`CREATE TABLE Persons (
+    PersonID int,
+    LastName varchar(255),
+    FirstName varchar(255),
+    Address varchar(255),
+    City varchar(255)
+);`, (err, res) => {
+    if(err){console.log("ERRRR"+err)}
+    else{console.log(res)}
+})
+
+module.exports = sql;
---
+++ routes/get.js
@@ -0,0 +1,34 @@
+module.exports = app => {;
+    var router = require("express").Router();
+
+
+  // test 
+  const jwt = require('jsonwebtoken');
+    const test1 = (req,res,next) =>{
+      res.status(200).send({
+        token: jwt.sign({id:7},'SECRET',{expiresIn:'24h'})
+      })
+    }
+
+    const test2 = (req,res,next) =>{
+
+          const token = req.headers.authorization.split(' ')[1];
+          const decodedToken = jwt.verify(token, 'SECRET');
+          const userId = decodedToken.id;
+          console.log(userId);
+      }
+
+
+
+    router.get('/client/test1',test1)
+    router.get('/client/test2',test2)    
+// 
+    //Client : 
+    //router.get('/client/connectiontatut',test)
+
+
+  
+  
+  
+    app.use('/get', router);
+  };  
